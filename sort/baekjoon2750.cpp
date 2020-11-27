1c70569961a97b3ed01a88de59fc6ef859465c5a
SonTaegyun <sontg123@naver.com>
2020-11-27 13:16:58 +0900

    basic sort

---
+++ sort/baekjoon2750.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class sorting{
+private:
+    vector<int> num;
+    int N;
+public :
+    sorting(int n):N(n){
+        num.resize(n);
+      
+    };    
+
+    void set_data(){
+        for(int i=0;i<N;i++)
+            std::cin >> num[i];
+    };
+    void bubble_sort(){
+        int tmp;
+        for(int i=0;i<N-1;i++){
+            for(int j=0;j<N-1-i;j++)
+                if(num[j]>num[j+1]){
+                    tmp=num[j+1];
+                    num[j+1]=num[j];
+                    num[j]=tmp;
+                }
+        }
+    };
+    void print_num(){
+        for(int i=0;i<N;i++)
+            std::cout << num[i]<<'\n';
+
+    };
+};
+
+int main(){
+    int n;
+    std::cin >>n;
+    sorting arr(n);
+    arr.set_data();
+    arr.bubble_sort();
+    arr.print_num();
+    
+    
+
+}
