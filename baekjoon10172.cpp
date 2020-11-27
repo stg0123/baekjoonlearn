20f4dff0f2f73b51a1fd00d03135970a521fe1ca
SonTaegyun <sontg123@naver.com>
2020-11-27 12:32:02 +0900

    Delete baekjoon10172.cpp

--- baekjoon10172.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-int main(){
-    std::cout << "|\\_/|"<<endl<<"|q p|   /}" << endl << "( 0 )\"\"\"\\"<<endl;
-    std::cout << "|\"^\"`    |"<<endl << "||_/=\\\\__|";
-
-    return 0;
-}
