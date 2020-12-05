7161fab4526c4f33688b33153e214cf1d53e109a
SonTaegyun <sontg123@naver.com>
2020-12-05 16:59:04 +0900

    basic pi

---
+++ mathematics/baekjoon3053.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+#include <math.h>
+
+const double pi = 3.141592653589;
+using namespace std;
+int main(){
+    double r;
+    std::cin >> r;
+    
+    printf("%.6f\n%.6f",pi*r*r,2*r*r);
+
+
+}
